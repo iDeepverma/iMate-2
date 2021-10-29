46ea3c23ef717071aa4007b8ce31c43973176a7e
Deepanshu <73387559+iDeepverma@users.noreply.github.com>
2021-10-29 19:01:01 +0530

    added message model and chat url

--- imate/chats/views.py
+++ imate/chats/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from django.contrib.auth import get_user_model
+from . import models
 # Create your views here.
+
+def chatView(request,username):
+    # try:
+    #     frnd = get_user_model().get(username =  username)
+    # except get_user_model().DoesNotExist:
+    #     # error handling if no such username exits
+    context = {}
+    context['frnd'] = get_object_or_404(get_user_model(),username=username)
+    convHash = models.conversastionhash(request.user,context['frnd'])
+    context['messages'] = models.Message.objects.filter(userHash=convHash).order_by('-timestamp')
+    context['frndProfile'] = context['frnd'].profile
+
+    return render(request,'chats/main-chat-page.html',context)
+    
+    
