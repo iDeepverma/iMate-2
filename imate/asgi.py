c7f6ca60f7693679006ec5ee7f6f40293a768e5d
Deepanshu <73387559+iDeepverma@users.noreply.github.com>
2021-11-30 22:28:14 +0530

    changes for deployment

--- imate/asgi.py
+++ imate/asgi.py
@@ -1,13 +1,13 @@
 # mysite/asgi.py
 import os
-
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chats.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imate.settings")
-
+django.setup()
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
