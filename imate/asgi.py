be00ffd7afd1da87a2492cde34bedeb54fc7094c
Deepanshu <73387559+iDeepverma@users.noreply.github.com>
2021-11-30 23:50:54 +0530

    changes for deployment

--- imate/asgi.py
+++ imate/asgi.py
@@ -1,20 +1,28 @@
 # mysite/asgi.py
+# import os
+# import django
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# import chats.routing
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imate.settings")
+# django.setup()
+# django_asgi_app = get_asgi_application()
+
+# application = ProtocolTypeRouter({
+#   "http": django_asgi_app,
+#   "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             chats.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
+
 import os
 import django
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chats.routing
+from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imate.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourappname.settings")
 django.setup()
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter({
-  "http": django_asgi_app,
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chats.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_default_application()
