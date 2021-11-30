d2c687aa5fd3c9c4195a0c4903fa4356b1311f8d
Deepanshu <73387559+iDeepverma@users.noreply.github.com>
2021-11-30 23:58:19 +0530

    changes for deployment

--- imate/asgi.py
+++ imate/asgi.py
@@ -21,8 +21,8 @@
 
 import os
 import django
-from channels.routing import get_default_application
+import channels.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourappname.settings")
 django.setup()
-application = get_default_application()
+application = channels.routing.get_default_application()
